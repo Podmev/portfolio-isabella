18d9324fe76ac0a5cc19aee57a8682ce58e7475c
Dmitriy Pozdin <podmev@gmail.com>
2024-10-28 01:56:58 +0100

    fetch articles in articles page

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,3 +1,9 @@
-export default function Page(){
+import axiosWithUrl from "../_lib/axiosWithUrl";
+
+export default async function Page(){
+    const data = await axiosWithUrl.get("/api/articles")
+    const articles = await data.data;
+    console.log(articles);
     return <h1>Articles page</h1>
+
 }
