d72fc9aa190dd5786d5d83734ed5ade81c7753b8
Dmitriy Pozdin <podmev@gmail.com>
2024-11-22 23:27:01 -0400

    first versions of colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +7,32 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: "#E1E8EF",
+          100: "#D4DEE7",
+          200: "#B7C7D7",
+          300: "#99B0C7",
+          400: "#7C99B6",
+          500: "#5E82A6",
+          600: "#4C6B8A",
+          700: "#3C546C",
+          800: "#2C3D4F",
+          900: "#1B2631",
+          950: "#141C24",
+        },
+        accent: {
+          50: "#fdf9e6",
+          100: "#faecb4",
+          200: "#f9e59b",
+          300: "#f5d968",
+          400: "#f2cc36",
+          500: "#efbf04",
+          600: "#d7ac04",
+          700: "#a78603",
+          800: "#786002",
+          900: "#483901",
+          950: "#302601",
+        },
       },
     },
   },
