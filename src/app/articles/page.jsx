f2f6c1348160a4a5058e71a980652f9b5e95a604
Dmitriy Pozdin <podmev@gmail.com>
2024-12-30 11:06:25 -0400

    remove articles page styles

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -3,7 +3,7 @@ import ArticleCardList from "@/components/ArticleCardList";
 export default async function Page(props) {
   const searchParams = await props.searchParams;
   return (
-    <div className="bg-primary-500 text-red-500">
+    <div className="">
       <h1>Articles page</h1>
       <ArticleCardList searchParams={searchParams} />
     </div>
