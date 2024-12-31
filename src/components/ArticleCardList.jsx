48cc1984bbcda196bb4fd4f6117adfeb1b11bcca
Dmitriy Pozdin <podmev@gmail.com>
2024-12-31 12:16:10 -0400

    customization article grid by screens: different num of columns

--- src/components/ArticleCardList.jsx
+++ src/components/ArticleCardList.jsx
@@ -5,7 +5,8 @@ import { getArticles } from "@/lib/data-service";
 async function ArticlesCardList({ searchParams }) {
   const articles = await getArticles(searchParams);
   return (
-    <div className="grid grid-cols-2 gap-8">
+    <div className="grid md:grid-cols-1 lg:grid-cols-2 
+    xl:grid-cols-3 2xl:grid-cols-4 gap-8">
       {articles.map((a) => (
         <ArticleCard article={a} key={a.name} />
       ))}
